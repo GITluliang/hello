340cc7918ac497d429b80fe369bd3ef3536c2664
luliang <luliang@git.com>
2019-05-19 22:53:18 +0800

    世界

--- src/main/java/cn/ll/servlet/HelloServlet.java
+++ src/main/java/cn/ll/servlet/HelloServlet.java
@@ -20,6 +20,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("当前时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:sss").format(new Date()));
+        System.out.println("世界你好！");
     }
 
     @Override
