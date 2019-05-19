4dacfbce9c443bfe24d9eb1c5e31524035e3df5f
luliang <luliang@git.com>
2019-05-19 20:54:00 +0800

    init

---
+++ src/main/java/cn/ll/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package cn.ll.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Auther: LL
+ * @Date: 2019/5/19 20:40
+ * @Description:
+ */
+@WebServlet("/hello.action")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("当前时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:sss").format(new Date()));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LL
+  Date: 2019/5/19
+  Time: 20:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <jsp:forward page="/hello.action"></jsp:forward>
+</body>
+</html>
